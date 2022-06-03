b97457877f99adaa53132cc322ae9a6ff45fc4e4
Dax Solomon Umaming <dax.umaming@beneco.com.ph>
2022-06-03 09:36:06 +0800

    update mysql.data nuget package, update settings

--- AGMARaffle/Properties/Settings.Designer.cs
+++ AGMARaffle/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace AGMARaffle.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,8 +25,7 @@ namespace AGMARaffle.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Server=api.beneco.com.ph;PORT=3306;Database=agmaraffle;Uid=agma;Pwd=!Agma1;SslMod" +
-            "e=none;")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Server=localhost;PORT=3306;Database=agmaraffle;Uid=agma;Pwd=!Agma1;SslMode=none;")]
         public string ConnectionString {
             get {
                 return ((string)(this["ConnectionString"]));
