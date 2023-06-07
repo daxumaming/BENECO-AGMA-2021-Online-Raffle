477b56a202a6b26bef6c28413dc2e2a54d58f642
Dax Solomon Umaming <knightlust@ubuntu.com>
2023-06-07 14:35:52 +0800

    update to prioritize district based on query on table

--- AGMARaffle/MainWindow.xaml.cs
+++ AGMARaffle/MainWindow.xaml.cs
@@ -126,17 +126,60 @@ namespace AGMARaffle
             }
             else
             {
+                // set selectedPrize variable
+                String selectedPrize = prizeDropdown.SelectedItem.ToString();
+
+                // counter
+                int otherdist_count = 0;
+                int prefdist = 0;
+
+                // default label content
                 txt_winners.Content = "searching....";
                 txt_prize.Content = "";
                 txt_stubno.Content = "";
 
-                var wheelWindow = new wheel();
-                wheelWindow.Show();
+                //var wheelWindow = new wheel();
+                //wheelWindow.Show();
 
                 // wait three seconds
-                await Task.Delay(5000);
+                //await Task.Delay(5000);
 
-                wheelWindow.Close();
+                //wheelWindow.Close();
+
+                using (MySqlConnection rafDistrict_conn = new MySqlConnection(Properties.Settings.Default.ConnectionString))
+                {
+                    try
+                    {
+                        rafDistrict_conn.Open();
+
+                        String rafDistrict_sql =
+                            "SELECT preferreddistrict, otherdist_count FROM prizes " +
+                            "WHERE prize_name = @selectedPrize";
+
+                        using var rafDistrict_cmd = new MySqlCommand(rafDistrict_sql, rafDistrict_conn);
+                        rafDistrict_cmd.Parameters.AddWithValue("@selectedPrize", selectedPrize);
+
+                        using MySqlDataReader rafDistrict_rdr = rafDistrict_cmd.ExecuteReader();
+                        rafDistrict_rdr.Read();
+
+                        if (rafDistrict_rdr.HasRows)
+                        {
+                            // set variable
+                            prefdist = rafDistrict_rdr.GetInt32(0);
+                            otherdist_count = rafDistrict_rdr.GetInt32(1);
+
+                        }
+                    }
+                    catch (MySqlException ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                    finally
+                    {
+                        rafDistrict_conn.Close();
+                    }
+
+                }
 
                 // initialize and open database
                 using (MySqlConnection rafDraw_conn = new MySqlConnection(Properties.Settings.Default.ConnectionString))
@@ -144,15 +187,32 @@ namespace AGMARaffle
                     try
                     {
                         rafDraw_conn.Open();
+                        String rafDraw_sql = "";
 
-                        // fetch random record from database
-                        String rafDraw_sql =
-                            "SELECT id, stubno, mconame FROM registeredmcos " +
+                        // fetch random record based on preferred district
+                        if (otherdist_count > 0)
+                        {
+                            rafDraw_sql =
+                            "SELECT id, stubno, mconame, district FROM registeredmcos " +
                             "WHERE flag_winner IS NULL " +
                             "ORDER BY RAND() LIMIT 1 ";
+                        } else
+                        {
+                            rafDraw_sql =
+                            "SELECT id, stubno, mconame, district FROM registeredmcos " +
+                            "WHERE district = @district " +
+                            "AND flag_winner IS NULL " +
+                            "ORDER BY RAND() LIMIT 1 ";
+                        }
 
-
+                        // fetch random record from database
                         using var rafDraw_cmd = new MySqlCommand(rafDraw_sql, rafDraw_conn);
+
+                        if (otherdist_count == 0)
+                        {
+                            rafDraw_cmd.Parameters.AddWithValue("@district", prefdist);
+                        }
+
                         using MySqlDataReader rafDraw_rdr = rafDraw_cmd.ExecuteReader();
                         rafDraw_rdr.Read();
 
@@ -160,16 +220,17 @@ namespace AGMARaffle
                         {
 
                             // set variable
-                            String selectedPrize = prizeDropdown.SelectedItem.ToString();
                             String id = rafDraw_rdr.GetString(0);
                             String stubno = rafDraw_rdr.GetString(1);
                             String mconame = rafDraw_rdr.GetString(2);
+                            String district = rafDraw_rdr.GetString(3);
 
                             // show results
                             txt_winners.Content = mconame;
                             txt_stubno.Content = "Stub No. " + stubno;
                             txt_prize.Content = selectedPrize;
                             txt_id.Content = id;
+                            txt_otherdist_count.Content = otherdist_count;
                             listResults.Items.Add(new { stubno = stubno, mconame = mconame, prize = selectedPrize });
 
 
@@ -238,32 +299,90 @@ namespace AGMARaffle
 
         private void claimButton_click(object sender, RoutedEventArgs e)
         {
-            // update prize table
-            using (MySqlConnection prizeUpd_conn = new MySqlConnection(Properties.Settings.Default.ConnectionString))
+            // variable for id label
+            String id = txt_id.Content.ToString();
+
+            if (id == null || id == "")
             {
-                String selectedPrize = prizeDropdown.SelectedItem.ToString();
-                String id = txt_id.Content.ToString();
+                MessageBox.Show("Please draw first.");
 
-
-                // update claimed flag
-                using (MySqlConnection rafUpd_conn = new MySqlConnection(Properties.Settings.Default.ConnectionString))
+            } else
+            {
+                // update prize table
+                using (MySqlConnection prizeUpd_conn = new MySqlConnection(Properties.Settings.Default.ConnectionString))
                 {
+                    String selectedPrize = prizeDropdown.SelectedItem.ToString();
+                    String otherdist_count_txt = txt_otherdist_count.Content.ToString();
+                    int otherdist_count = int.Parse(otherdist_count_txt);
+
+
+                    // update claimed flag
+                    using (MySqlConnection rafUpd_conn = new MySqlConnection(Properties.Settings.Default.ConnectionString))
+                    {
+                        try
+                        {
+                            rafUpd_conn.Open();
+
+                            // update record, set flag to true
+                            String dtnow = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                            String rafUpd_sql =
+                                "UPDATE registeredmcos " +
+                                "SET claimed = 1 " +
+                                "WHERE id = " + id;
+
+                            using var rafUpd_cmd = new MySqlCommand();
+                            rafUpd_cmd.Connection = rafUpd_conn;
+                            rafUpd_cmd.CommandText = rafUpd_sql;
+                            rafUpd_cmd.Parameters.AddWithValue("@selectedPrize", selectedPrize);
+                            rafUpd_cmd.ExecuteNonQuery();
+                        }
+                        catch (MySqlException ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                        finally
+                        {
+                            rafUpd_conn.Close();
+                        }
+
+                    }
+
+
+                    // update prize issued
                     try
                     {
-                        rafUpd_conn.Open();
+                        prizeUpd_conn.Open();
+                        String prizeUpd_sql = "";
 
-                        // update record, set flag to true
-                        String dtnow = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
-                        String rafUpd_sql =
-                            "UPDATE registeredmcos " +
-                            "SET claimed = 1 " +
-                            "WHERE id = " + id;
+                        if (otherdist_count > 0)
+                        {
+                            prizeUpd_sql =
+                            "UPDATE prizes " +
+                            "SET issued = issued + 1, " +
+                            "otherdist_count = @otherdist_count " +
+                            "WHERE prize_name = @prizeName ";
+                        }
+                        else
+                        {
+                            prizeUpd_sql =
+                            "UPDATE prizes " +
+                            "SET issued = issued + 1 " +
+                            "WHERE prize_name = @prizeName ";
+                        }
 
-                        using var rafUpd_cmd = new MySqlCommand();
-                        rafUpd_cmd.Connection = rafUpd_conn;
-                        rafUpd_cmd.CommandText = rafUpd_sql;
-                        rafUpd_cmd.Parameters.AddWithValue("@selectedPrize", selectedPrize);
-                        rafUpd_cmd.ExecuteNonQuery();
+                        using var prizeUpd_cmd = new MySqlCommand();
+                        prizeUpd_cmd.Connection = prizeUpd_conn;
+                        prizeUpd_cmd.CommandText = prizeUpd_sql;
+                        prizeUpd_cmd.Parameters.AddWithValue("@prizeName", selectedPrize);
+                        if (otherdist_count > 0)
+                        {
+                            prizeUpd_cmd.Parameters.AddWithValue("@otherdist_count", otherdist_count - 1);
+                        }
+                        prizeUpd_cmd.ExecuteNonQuery();
+
+                        Int32 prizeTotal = Convert.ToInt32(txt_prizeNo.Text);
+                        prizeTotal--;
+                        txt_prizeNo.Text = prizeTotal.ToString();
                     }
                     catch (MySqlException ex)
                     {
@@ -271,45 +390,11 @@ namespace AGMARaffle
                     }
                     finally
                     {
-                        rafUpd_conn.Close();
+                        prizeUpd_conn.Close();
                     }
-
-                }
-
-
-                // update prize issued
-                try
-                {
-                    prizeUpd_conn.Open();
-                    String prizeUpd_sql =
-                        "UPDATE prizes " +
-                        "SET issued = issued + 1 " +
-                        "WHERE prize_name = @prizeName ";
-                    using var prizeUpd_cmd = new MySqlCommand();
-                    prizeUpd_cmd.Connection = prizeUpd_conn;
-                    prizeUpd_cmd.CommandText = prizeUpd_sql;
-                    prizeUpd_cmd.Parameters.AddWithValue("@prizeName", selectedPrize);
-                    prizeUpd_cmd.ExecuteNonQuery();
-
-                    Int32 prizeTotal = Convert.ToInt32(txt_prizeNo.Text);
-                    prizeTotal--;
-                    txt_prizeNo.Text = prizeTotal.ToString();
-                }
-                catch (MySqlException ex)
-                {
-                    MessageBox.Show(ex.Message);
-                }
-                finally
-                {
-                    prizeUpd_conn.Close();
                 }
             }
-
         }
 
-        private void unclaimButton_click(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
